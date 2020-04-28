95f2a869452efefc5e7a1fd4c01aa78ff0458481
Matthias Osswald <mat.osswald@sap.com>
2020-04-28 16:19:46 +0200

    Upgrade to ava 3.8.1 / add more tests

--- test/lib/logger.js
+++ test/lib/logger.js
@@ -1,27 +1,99 @@
 const test = require("ava");
+const mock = require("mock-require");
+const sinon = require("sinon");
 
-const logger = require("../../lib/logger");
-const Logger = logger._Logger;
+test.beforeEach((t) => {
+	t.context.npmLogStub = {
+		enableProgress: sinon.stub(),
+		disableProgress: sinon.stub(),
+		enableUnicode: sinon.stub(),
+		on: sinon.stub(),
+		newGroup: sinon.stub(),
+	};
+	mock("npmlog", t.context.npmLogStub);
 
-test("getLogger", (t) => {
-	t.is(typeof logger.getLogger, "function");
+	t.context.logger = mock.reRequire("../../lib/logger");
+});
+
+test.afterEach.always((t) => {
+	mock.stopAll();
+	sinon.restore();
+});
+
+test.serial("getLogger", (t) => {
+	const {logger} = t.context;
+	const {Logger} = logger.__test__;
 
 	const myLogger = logger.getLogger("my-module");
 	t.true(myLogger instanceof Logger, "Returned logger should be Logger instance");
 });
 
-test("getGroupLogger", (t) => {
-	t.is(typeof logger.getGroupLogger, "function");
+test.serial("getGroupLogger", (t) => {
+	const {logger} = t.context;
+	const {GroupLogger} = logger.__test__;
+
+	const myLogger = logger.getGroupLogger("my-module");
+	t.true(myLogger instanceof GroupLogger, "Returned logger should be Logger instance");
 });
 
-test("setLevel", (t) => {
-	t.is(typeof logger.setLevel, "function");
+test.serial("setLevel", (t) => {
+	const {logger} = t.context;
+
+	logger.setLevel("silly");
+	t.is(t.context.npmLogStub.level, "silly", "npmlog.level should be set to 'silly'");
+
+	t.throws(() => logger.setLevel("fancy"), {message: `Unkown log level "fancy"`});
+	t.is(t.context.npmLogStub.level, "silly", "npmlog.level should still be set to 'silly'");
 });
 
-test("isLevelEnabled", (t) => {
-	t.is(typeof logger.isLevelEnabled, "function");
+test.serial("isLevelEnabled", (t) => {
+	const {logger} = t.context;
+
+	// On silly all levels should be enabled
+	logger.setLevel("silly");
+	t.true(logger.isLevelEnabled("silly"));
+	t.true(logger.isLevelEnabled("verbose"));
+	t.true(logger.isLevelEnabled("info"));
+	t.true(logger.isLevelEnabled("warn"));
+	t.true(logger.isLevelEnabled("error"));
+	t.true(logger.isLevelEnabled("silent"));
+
+	// On silent only silent should be enabled
+	logger.setLevel("silent");
+	t.false(logger.isLevelEnabled("silly"));
+	t.false(logger.isLevelEnabled("verbose"));
+	t.false(logger.isLevelEnabled("info"));
+	t.false(logger.isLevelEnabled("warn"));
+	t.false(logger.isLevelEnabled("error"));
+	t.true(logger.isLevelEnabled("silent"));
+
+	// On info only info,warn,error,silent should be enabled
+	logger.setLevel("info");
+	t.false(logger.isLevelEnabled("silly"));
+	t.false(logger.isLevelEnabled("verbose"));
+	t.true(logger.isLevelEnabled("info"));
+	t.true(logger.isLevelEnabled("warn"));
+	t.true(logger.isLevelEnabled("error"));
+	t.true(logger.isLevelEnabled("silent"));
 });
 
-test("setShowProgress", (t) => {
-	t.is(typeof logger.setShowProgress, "function");
+test.serial("setShowProgress", (t) => {
+	const {logger, npmLogStub} = t.context;
+
+	t.is(npmLogStub.enableProgress.callCount, 0, "enableProgress should not be called initially");
+	t.is(npmLogStub.disableProgress.callCount, 0, "disableProgress should not be called initially");
+
+	logger.setShowProgress(true);
+
+	t.is(npmLogStub.enableProgress.callCount, 1, "enableProgress should be called once");
+	t.is(npmLogStub.disableProgress.callCount, 0, "disableProgress should still not be called");
+
+	logger.setShowProgress(false);
+
+	t.is(npmLogStub.enableProgress.callCount, 1, "enableProgress should still be called once");
+	t.is(npmLogStub.disableProgress.callCount, 1, "disableProgress should be called once");
+});
+
+test.serial("npmlog.level default", (t) => {
+	t.is(t.context.npmLogStub.level, "info", "Default level should be info");
 });
