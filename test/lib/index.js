ac9d005a92704aa145293c34ea11be5ce5797714
Matthias Osswald <mat.osswald@sap.com>
2022-09-26 13:55:03 +0200

    Remove index.js

--- test/lib/index.js
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-
-test("index.js exports logger module", async (t) => {
-	const {default: index} = await import("../../index.js");
-	t.truthy(index, "Module exported");
-
-	const {default: logger} = await import("../../lib/logger.js");
-	t.is(index, logger, "Logger module exported");
-});
