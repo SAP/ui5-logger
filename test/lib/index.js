183dde0129c77e4e3cc23805ac57d8de2d92fb90
Florian Vogt <florian.vogt@sap.com>
2024-01-25 15:04:53 +0100

    [DO NOT MERGE] test for v2 pipeline

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,6 +1,7 @@
 const test = require("ava");
 
 test("index.js exports logger module", (t) => {
+	t.fail("test to fail");
 	const index = require("../../");
 	t.truthy(index, "Module exported");
 	t.is(index, require("../../lib/logger"), "Logger module exported");
